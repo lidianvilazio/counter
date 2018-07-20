93cfa96343f8af3a96504842852c037011840432
github username <github email address>
2018-07-20 17:00:06 -0400

    timer

---
+++ src/Components/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Counter extends React.Component {
+
+  state = {
+    num: 0,
+    clicked: false,
+  }
+
+  click = () => {
+    let num = this.state.num+1
+    setInterval(() => {this.setState({num: this.state.num+1})}, 1000)
+    this.setState({clicked: !this.state.clicked})
+  }
+
+  render() {
+    return(
+      <div className='main'>
+        {this.state.num > 0 ? this.state.num : null}<br/>
+        {this.state.clicked ? null : <button onClick={this.click}>Count</button>}
+      </div>
+    )
+  }
+}
+
+export default Counter
